3579e19f9c1f2d5b86d59a2c51289e62939cc512
BuDong <fish880111@163.com>
2016-08-26 11:20:20 +0800

    模板修改

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 import datetime
+
+from django.template import Template, Context
 
 
 def hello(request):
@@ -11,15 +13,22 @@ def error(request):
     return HttpResponse('<h1>Not Found!</h1>')
 
 
-def current_time(request):
+def current_time(request, ahead):
+    try:
+        ahead = int(ahead)
+    except ValueError:
+        raise Http404()
     now = datetime.datetime.now()
-    return HttpResponse('<html><body>now the time is %s!</body></html>' % now)
+    #return HttpResponse('<html><body>now the time is %s!</body></html>' % now)
+    a = {'now': now, 'ahead': ahead}
+    html = Template('<html><body>now the time is {{a.now}}! and it\'s ahead {{a.ahead}} hours</body></html>')
+    c = Context({'a': a})
+    a = html.render(c)
+    return HttpResponse(a)
 
 
 
 
 
 
-
-1231231233333333333333
 
